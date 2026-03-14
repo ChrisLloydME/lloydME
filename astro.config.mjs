2a85c7e633fbdc253c3cb2b8305fcb7870aa615d
Chris Lloyd <CLD_encryption@outlook.com>
2026-03-15 01:09:04 +0800

    fix minor issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://chrislloydme.github.io',
   base: '/lloydME',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
